9dff09ca4abea80027a09bd63803ce2bcf5062c5
Darin <darinchengg@gmail.com>
2022-05-27 15:56:21 -0500

    Write four basic calculator functions

---
+++ script.js
@@ -0,0 +1,32 @@
+/* 
+create operate function that takes an operator, two numbers
+and calls one of the functions on the numbers (do not allow division by 0)
+
+create function that populates display when number buttons are hit (DOM event listener)
+store display value as variable for further steps 
+
+store first number input when a user presses an operator, save operator chosen, and call operate() function 
+when equals button is pressed 
+
+allow for stringing of operators, basically store a running total number for further operations 
+
+round numbers with long decimals to prevent display overflow 
+
+possibly write a clear function that resets all input variables 
+*/
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
