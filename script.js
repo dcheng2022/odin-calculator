f85a0482e3a78cee43abd6e158ac859e8eea21c6
Darin <darinchengg@gmail.com>
2022-05-27 16:45:38 -0500

    Create display function that updates placeholder when operate is called

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 /* 
-create function that populates display when number buttons are hit (DOM event listener)
 store display value as variable for further steps 
 
 store first number input when a user presses an operator, save operator chosen, and call operate() function 
@@ -7,24 +6,29 @@ when equals button is pressed
 
 allow for stringing of operators, basically store a running total number for further operations 
 
-round numbers with long decimals to prevent display overflow 
-
 write a clear function that resets all input variables 
 */
+function display(string) {
+    /* executes upon events from keypad buttons 
+    and also the operate() function
+    which executes when "=" is pressed */
+    const stringContainer = document.querySelector('.text-container');
+    stringContainer.textContent = string;
+} 
+
 function operate(operator, num1, num2) {
-    // function return should be passed to display function
     switch (operator) {
         case "+": 
-            add(num1, num2);
+            display(add(num1, num2));
             break;
         case "-":
-            subtract(num1, num2);
+            display(subtract(num1, num2));
             break;
         case "*":
-            multiply(num1, num2);
+            display(multiply(num1, num2));
             break;
         case "/":
-            divide(num1, num2);
+            display(divide(num1, num2));
             break;
         default:
             break;
